d637a45a01879c0199848a6f1d7eefc7525d2664
Matias Senger <m.senger@hotmail.com>
2021-02-07 11:35:32 +0100

    new plot

---
+++ 210206 beta source vs TCT: Theory/beta_source_betagama_factor.py
@@ -0,0 +1,30 @@
+import numpy as np
+import myplotlib as mpl
+import scipy.constants as constants
+
+m_e_in_eV = constants.electron_mass*constants.c**2/constants.e # https://physics.nist.gov/cgi-bin/cuu/Convert?exp=0&num=1&From=kg&To=ev&Action=Convert+value+and+show+factor
+E = np.linspace(m_e_in_eV, 2.5e6, 99) # MeV
+
+gamma = E/m_e_in_eV 
+beta = (1-1/gamma**2)**.5
+
+fig = mpl.manager.new(
+	title = 'Relativistic factors for electrons emitted by a Sr-90 source',
+	xlabel = 'Energy (eV)',
+)
+# ~ fig.plot(
+	# ~ E,
+	# ~ gamma,
+	# ~ label = 'γ',
+# ~ )
+fig.plot(
+	E,
+	beta,
+	label = 'β',
+)
+fig.plot(
+	E,
+	gamma*beta,
+	label = 'βγ',
+)
+mpl.manager.save_all()
